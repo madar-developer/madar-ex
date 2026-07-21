47384434d24b7d01e1a39fe3bbc335294a8ae793
madar.developer <madar.developer@gmail.com>
2026-07-21 14:24:28 +0300

    seereer edit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,7 +21,7 @@ class Order extends Model
 
     public static function getLevels($status)
     {
-        $levels = [];<
+        $levels = [];
         switch ($status) {
             case 'new':
                 $levels = ['init', 'not_received'];
